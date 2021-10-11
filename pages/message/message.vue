fc11313d7280b53550db3661ce92964fea9a703d
xindan <2499232802@qq.com>
2021-10-11 15:48:32 +0800

    我的消息、足迹、积分开发

--- pages/message/message.vue
+++ pages/message/message.vue
@@ -1,213 +1,174 @@
 <template>
-<view>
-<scroll-view v-if="data_list.length > 0" :scroll-y="true" class="scroll-box" @scrolltolower="scroll_lower" lower-threshold="30">
-  <view class="content">
-    <view v-for="(item, index) in data_list" :key="index" class="data-card bg-white br-b">
-      <view class="data-box oh">
-        <text class="data-title">{{item.title}}</text>
-        <text class="data-time fr">{{item.add_time_time}}</text>
-      </view>
-      <view class="data-detail">{{item.detail}}</view>
+    <view>
+        <scroll-view :scroll-y="true" class="scroll-box" @scrolltolower="scroll_lower" lower-threshold="30">
+            <view v-if="data_list.length > 0" class="padding-main">
+                <view v-for="(item, index) in data_list" :key="index" class="padding-main border-radius-main bg-white oh spacing-mb">
+                    <view class="oh">
+                        <text class="fw-b">{{item.title}}</text>
+                        <text class="fr cr-base">{{item.add_time_time}}</text>
+                    </view>
+                    <view class="cr-gray margin-top-sm">{{item.detail}}</view>
+                </view>
+            </view>
+            <view v-else>
+                <!-- 提示信息 -->
+                <component-no-data :prop-status="data_list_loding_status"></component-no-data>
+            </view>
+
+            <!-- 结尾 -->
+            <component-bottom-line :prop-status="data_bottom_line_status"></component-bottom-line>
+        </scroll-view>
     </view>
-  </view>
-  <!--<import src="/pages/common/bottom_line.wxml"></import>-->
-  <block data-type="template" data-is="bottom_line" data-attr="status: data_bottom_line_status">
-    <view v-if="(status || false)" class="data-bottom-line">
-        <view class="left fl"></view>
-        <view class="msg fl">我是有底线的</view>
-        <view class="right fr"></view>
-    </view>
-</block>
-</scroll-view>
-
-<view v-if="data_list.length == 0">
-  <!--<import src="/pages/common/nodata.wxml"></import>-->
-  <block data-type="template" data-is="nodata" data-attr="status: data_list_loding_status">
-  <!-- 1 加载中 -->
-  <view v-if="0 == 1" class="no-data-loding tc">
-    <text>加载中...</text>
-  </view>
-
-  <!-- 2 处理错误 -->
-  <view v-else-if="0 == 2" class="no-data-box tc">
-    <image src="/static/images/error.png" mode="widthFix"></image>
-    <view class="no-data-tips">{{msg || '处理错误'}}</view>
-  </view>
-
-  <!-- 0 默认没有数据 -->
-  <view v-else-if="0 == 0" class="no-data-box tc">
-    <image src="/static/images/empty.png" mode="widthFix"></image>
-    <view class="no-data-tips">{{msg || '没有相关数据'}}</view>
-  </view>
-</block>
-</view>
-</view>
 </template>
 
 <script>
-const app = getApp();
+    const app = getApp();
+    import componentNoData from "../../components/no-data/no-data";
+    import componentBottomLine from "../../components/bottom-line/bottom-line";
 
-export default {
-  data() {
-    return {
-      data_list: [],
-      data_page_total: 0,
-      data_page: 1,
-      data_list_loding_status: 1,
-      data_bottom_line_status: false,
-      data_total: ""
-    };
-  },
-
-  components: {},
-  props: {},
-
-  onShow() {
-    uni.setNavigationBarTitle({
-      title: app.globalData.data.common_pages_title.message
-    });
-    this.init();
-  },
-
-  // 下拉刷新
-  onPullDownRefresh() {
-    this.setData({
-      data_page: 1
-    });
-    this.get_data_list(1);
-  },
-
-  methods: {
-    init() {
-      var user = app.globalData.get_user_info(this, "init");
-
-      if (user != false) {
-        // 用户未绑定用户则转到登录页面
-        if (app.globalData.user_is_need_login(user)) {
-          uni.redirectTo({
-            url: "/pages/login/login?event_callback=init"
-          });
-          return false;
-        } else {
-          // 获取数据
-          this.get_data_list();
-        }
-      } else {
-        this.setData({
-          data_list_loding_status: 0,
-          data_bottom_line_status: false
-        });
-      }
-    },
-
-    get_data_list(is_mandatory) {
-      // 分页是否还有数据
-      if ((is_mandatory || 0) == 0) {
-        if (this.data_bottom_line_status == true) {
-          return false;
-        }
-      } else {
-        this.setData({
-          data_bottom_line_status: false
-        });
-      } // 加载loding
-
-
-      uni.showLoading({
-        title: "加载中..."
-      });
-      this.setData({
-        data_list_loding_status: 1
-      }); // 获取数据
-
-      uni.request({
-        url: app.globalData.get_request_url("index", "message"),
-        method: "POST",
-        data: {
-          page: this.data_page
+    export default {
+        data() {
+            return {
+                data_list: [],
+                data_total: 0,
+                data_page_total: 0,
+                data_page: 1,
+                data_list_loding_status: 1,
+                data_bottom_line_status: false
+            };
         },
-        dataType: "json",
-        header: {
-          'content-type': 'application/x-www-form-urlencoded'
+
+        components: {
+            componentNoData,
+            componentBottomLine
         },
-        success: res => {
-          uni.hideLoading();
-          uni.stopPullDownRefresh();
+        props: {},
 
-          if (res.data.code == 0) {
-            if (res.data.data.data.length > 0) {
-              if (this.data_page <= 1) {
-                var temp_data_list = res.data.data.data;
-              } else {
-                var temp_data_list = this.data_list;
-                var temp_data = res.data.data.data;
-
-                for (var i in temp_data) {
-                  temp_data_list.push(temp_data[i]);
-                }
-              }
-
-              this.setData({
-                data_list: temp_data_list,
-                data_total: res.data.data.total,
-                data_page_total: res.data.data.page_total,
-                data_list_loding_status: 3,
-                data_page: this.data_page + 1
-              }); // 是否还有数据
-
-              if (this.data_page > 1 && this.data_page > this.data_page_total) {
-                this.setData({
-                  data_bottom_line_status: true
-                });
-              }
-            } else {
-              this.setData({
-                data_list_loding_status: 0
-              });
-            }
-          } else {
-            this.setData({
-              data_list_loding_status: 0
+        onShow() {
+            uni.setNavigationBarTitle({
+                title: app.globalData.data.common_pages_title.message
             });
-
-            if (app.globalData.is_login_check(res.data, this, 'get_data_list')) {
-              app.globalData.showToast(res.data.msg);
-            }
-          }
+            this.init();
         },
-        fail: () => {
-          uni.hideLoading();
-          uni.stopPullDownRefresh();
-          this.setData({
-            data_list_loding_status: 2
-          });
-          app.globalData.showToast("服务器请求出错");
+
+        // 下拉刷新
+        onPullDownRefresh() {
+            this.setData({
+                data_page: 1
+            });
+            this.get_data_list(1);
+        },
+
+        methods: {
+            init() {
+                var user = app.globalData.get_user_info(this, "init");
+                if (user != false) {
+                    // 用户未绑定用户则转到登录页面
+                    if (app.globalData.user_is_need_login(user)) {
+                        uni.redirectTo({
+                            url: "/pages/login/login?event_callback=init"
+                        });
+                        return false;
+                    } else {
+                        // 获取数据
+                        this.get_data_list();
+                    }
+                } else {
+                    this.setData({
+                        data_list_loding_status: 0,
+                        data_bottom_line_status: false
+                    });
+                }
+            },
+
+            get_data_list(is_mandatory) {
+                // 分页是否还有数据
+                if ((is_mandatory || 0) == 0) {
+                    if (this.data_bottom_line_status == true) {
+                        return false;
+                    }
+                } else {
+                    this.setData({
+                        data_bottom_line_status: false
+                    });
+                }
+                
+                // 加载loding
+                uni.showLoading({
+                    title: "加载中..."
+                });
+                this.setData({
+                    data_list_loding_status: 1
+                });
+                
+                // 获取数据
+                uni.request({
+                    url: app.globalData.get_request_url("index", "message"),
+                    method: "POST",
+                    data: {
+                        page: this.data_page
+                    },
+                    dataType: "json",
+                    success: res => {
+                        uni.hideLoading();
+                        uni.stopPullDownRefresh();
+                        if (res.data.code == 0) {
+                            if (res.data.data.data.length > 0) {
+                                if (this.data_page <= 1) {
+                                    var temp_data_list = res.data.data.data;
+                                } else {
+                                    var temp_data_list = this.data_list;
+                                    var temp_data = res.data.data.data;
+                                    for (var i in temp_data) {
+                                        temp_data_list.push(temp_data[i]);
+                                    }
+                                }
+
+                                this.setData({
+                                    data_list: temp_data_list,
+                                    data_total: res.data.data.total,
+                                    data_page_total: res.data.data.page_total,
+                                    data_list_loding_status: 3,
+                                    data_page: this.data_page + 1
+                                });
+                                
+                                // 是否还有数据
+                                if (this.data_page > 1 && this.data_page > this.data_page_total) {
+                                    this.setData({
+                                        data_bottom_line_status: true
+                                    });
+                                }
+                            } else {
+                                this.setData({
+                                    data_list_loding_status: 0
+                                });
+                            }
+                        } else {
+                            this.setData({
+                                data_list_loding_status: 0
+                            });
+                            if (app.globalData.is_login_check(res.data, this, 'get_data_list')) {
+                                app.globalData.showToast(res.data.msg);
+                            }
+                        }
+                    },
+                    fail: () => {
+                        uni.hideLoading();
+                        uni.stopPullDownRefresh();
+                        this.setData({
+                            data_list_loding_status: 2
+                        });
+                        app.globalData.showToast("服务器请求出错");
+                    }
+                });
+            },
+
+            // 滚动加载
+            scroll_lower(e) {
+                this.get_data_list();
+            }
         }
-      });
-    },
-
-    // 滚动加载
-    scroll_lower(e) {
-      this.get_data_list();
-    }
-
-  }
-};
+    };
 </script>
 <style>
-.data-card {
-    padding: 30rpx 20rpx;
-    font-size: 24rpx;
-    color: #a6a6a6;
-}
-.data-card .data-box {
-    margin-bottom: 20rpx;
-}
-.data-card .data-title {
-    font-size: 28rpx;
-    color: #4a4a4a;
-}
-.data-card .data-detail {
-    line-height: 36rpx;
-}
 </style>
