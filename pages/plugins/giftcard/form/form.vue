255a766cc3961aaf27ef422d5d88e7723f509c5e
gongfuxiang <fuxiang.gong@qq.com>
2024-07-09 15:58:05 +0800

    多商户和门店新增核销

--- pages/plugins/giftcard/form/form.vue
+++ pages/plugins/giftcard/form/form.vue
@@ -7,13 +7,17 @@
                         <view class="title fw-b text-size margin-vertical-xxxl">{{$t('giftcard-index.giftcard-index.hfg2fg')}}</view>
                         <view class="flex-row align-c padding-bottom-xl">
                             <view class="margin-right" @tap="scan_event">
-                                <uni-icons type="scan" size="42rpx" color="#666"></uni-icons>
+                                <uni-icons type="scan" size="56rpx" color="#666"></uni-icons>
                             </view>
-                            <input type="text" class="text-size-lg wh-auto" :placeholder="$t('giftcard-index.giftcard-index.fu3rf1')" placeholder-class="cr-grey-c" :value="secret_key_value" @input="secret_key_event" />
+                            <input type="text" class="wh-auto secret-key-value" :placeholder="$t('giftcard-index.giftcard-index.fu3rf1')" placeholder-class="cr-grey-c" :value="secret_key_value" @input="secret_key_event" />
                         </view>
                     </view>
                     <view class="padding-main">
-                        <button type="default" form-type="submit" hover-class="none" size="mini" class="br-main bg-main cr-white round buy-submit text-size dis-block" :disabled="form_submit_loading">{{$t('common.confirm')}}</button>
+                        <button type="default" form-type="submit" hover-class="none" class="br-main bg-main cr-white round text-size-lg" :disabled="form_submit_loading">{{$t('common.confirm')}}</button>
+                    </view>
+                    <view class="padding-lg margin-top-xl text-size-lg tc">
+                        <text v-if="(error_msg || null) != null" class="cr-red">{{error_msg}}</text>
+                        <text v-if="(success_msg || null) != null" class="cr-green">{{success_msg}}</text>
                     </view>
                 </form>
             </block>
@@ -33,7 +37,9 @@
                 data_list_loding_status: 1,
                 data_list_loding_msg: '',
                 form_submit_loading: false,
-                secret_key_value: ''
+                secret_key_value: '',
+                error_msg: '',
+                success_msg: ''
             };
         },
         components: {
@@ -93,6 +99,10 @@
 
             // 表单提交
             form_submit() {
+                this.setData({
+                    error_msg: '',
+                    success_msg: ''
+                });
                 var form_data = {
                     secret_key: this.secret_key_value
                 }
@@ -114,11 +124,14 @@
                         success: (res) => {
                             uni.hideLoading();
                             if (res.data.code == 0) {
-                                app.globalData.showToast(res.data.msg, 'success');
                                 var self = this;
+                                self.setData({
+                                    error_msg: '',
+                                    success_msg: res.data.msg,
+                                });
                                 setTimeout(function() {
                                     self.setData({
-                                        form_submit_loading: false
+                                        form_submit_loading: false,
                                     });
                                     if(app.globalData.prev_page() == null) {
                                         app.globalData.url_open('/pages/plugins/giftcard/index/index', true);
@@ -129,18 +142,20 @@
                             } else {
                                 if (app.globalData.is_login_check(res.data, this, 'form_submit')) {
                                     this.setData({
-                                        form_submit_loading: false
+                                        form_submit_loading: false,
+                                        error_msg: res.data.msg,
+                                        success_msg: '',
                                     });
-                                    app.globalData.showToast(res.data.msg);
                                 }
                             }
                         },
                         fail: () => {
-                            this.setData({
-                                form_submit_loading: false
-                            });
                             uni.hideLoading();
-                            app.globalData.showToast(this.$t('common.internet_error_tips'));
+                            this.setData({
+                                form_submit_loading: false,
+                                error_msg: this.$t('common.internet_error_tips'),
+                                success_msg: '',
+                            });
                         },
                     });
                 }
@@ -148,5 +163,10 @@
         }
     };
 </script>
-<style>
+<style scoped>
+    input.secret-key-value {
+        height: 100rpx;
+        line-height: 100rpx;
+        font-size: 44rpx;
+    }
 </style>
