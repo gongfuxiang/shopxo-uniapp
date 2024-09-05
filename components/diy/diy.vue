742a7c5008c99e7555ff3678ad43f573133a91fe
sws <1141121512@qq.com>
2024-09-05 17:31:04 +0800

    进销存: 1.diy装修渲染布局 sws-2024-09-05

---
+++ components/diy/diy.vue
@@ -0,0 +1,33 @@
+<template>
+    <view>
+        <view v-if="value.diy_data.length > 0">
+            <componentDiySearch></componentDiySearch>
+            <view v-for="(item, index) in value.diy_data" :key="index">
+                <componentDiyAuxiliaryLine v-if="item.key == 'row-line'" :value="item.com_data"></componentDiyAuxiliaryLine>
+            </view>
+        </view>
+    </view>
+</template>
+
+<script>
+    import componentDiySearch from '@/components/diy/search';
+    import componentDiyAuxiliaryLine from '@/components/diy/auxiliary-line';
+    export default {
+        name: 'diy',
+        props: {
+            value: {
+                type: Object,
+                default: () => ({}),
+            },
+        },
+        components: {
+            componentDiySearch,
+            componentDiyAuxiliaryLine,
+        },
+        data() {
+            return {};
+        },
+    };
+</script>
+
+<style></style>
---
+++ pages/diy/diy.vue
@@ -0,0 +1,26 @@
+<template>
+    <view>
+        <componentDiy :value="diy_data"></componentDiy>
+    </view>
+</template>
+
+<script>
+    import componentDiy from '@/components/diy/diy';
+    import initData from './init_data';
+    export default {
+        components: {
+            componentDiy,
+        },
+        data() {
+            return {
+                diy_data: initData,
+            };
+        },
+        mounted() {
+            console.log(initData);
+        },
+        methods: {},
+    };
+</script>
+
+<style></style>
