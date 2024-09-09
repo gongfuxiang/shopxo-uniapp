206cabfd3b02e7e0aa2d7fdba11148982059df14
sws <1141121512@qq.com>
2024-09-09 15:16:38 +0800

    进销存: 1.diy装修渲染布局 sws-2024-09-09

--- components/diy/footer.vue
+++ components/diy/footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <view class="footer-nav flex-row jc-c align-c" :class="showFooter ? 'br-2 br-primary' : ''" @click="footer_nav_event">
+    <view class="footer-nav flex-row jc-c align-c">
         <view class="footer-nav-content flex-row jc-c align-c w" :style="style_container">
             <ul class="flex-row jc-sa align-c w">
                 <li v-for="(item, index) in nav_content" :key="index" class="flex-1 flex-col jc-c align-c gap-5" @mouseenter="is_hover = index" @mouseleave="is_hover = 0">
@@ -19,11 +19,46 @@
 </template>
 
 <script>
+    import { common_styles_computer } from '@/common/js/common/common.js';
     export default {
-        data() {
-            return {};
+        props: {
+            value: {
+                type: Object,
+                default: () => ({}),
+            },
         },
-        methods: {},
+        data() {
+            return {
+                style_container: '',
+                style: '',
+                nav_content: [],
+                nav_style: 0,
+                default_text_color: '',
+                text_color_checked: '',
+            };
+        },
+        mounted() {
+            init();
+        },
+        methods: {
+            init() {
+                const new_content = value.content || {};
+                const new_style = value.style || {};
+                this.nav_content = new_content.nav_content || [];
+                this.nav_style = new_content.nav_style || 0;
+                this.default_text_color = 'color:' + new_style.default_text_color || 'rgba(0, 0, 0, 1)';
+                this.text_color_checked = 'color:' + new_style.text_color_checked || 'rgba(204, 204, 204, 1)';
+
+                this.style_container = common_styles_computer(new_style.common_style);
+                let footer_height = new_style.common_style.padding_top + new_style.common_style.padding_bottom + new_style.common_style.margin_top + new_style.common_style.margin_bottom + 50;
+                if (footer_height >= 70) {
+                    footer_height = footer_height;
+                } else {
+                    footer_height = 70;
+                }
+                footer_nav_counter_store.padding_footer_computer(footer_height);
+            },
+        },
     };
 </script>
 
