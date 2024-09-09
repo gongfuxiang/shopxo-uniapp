ac46edf680d7d2b2378af25d33933f4d53f9c08a
sws <1141121512@qq.com>
2024-09-09 18:00:35 +0800

    1.diy---- 富文本开发

--- components/diy/diy.vue
+++ components/diy/diy.vue
@@ -3,10 +3,13 @@
         <componentDiyHeader></componentDiyHeader>
         <view v-if="value.diy_data.length > 0" :style="diy_content_style">
             <view v-for="(item, index) in value.diy_data" :key="index">
+                <!-- 基础组件 -->
                 <componentDiySearch></componentDiySearch>
-                <componentDiyAuxiliaryLine v-if="item.key == 'row-line'" :value="item.com_data"></componentDiyAuxiliaryLine>
+                <!-- 工具组件 -->
                 <componentFloatWindow v-if="item.key == 'float-window'" :value="item.com_data"></componentFloatWindow>
                 <componentTextTitle v-if="item.key == 'text-title'" :value="item.com_data"></componentTextTitle>
+                <componentDiyAuxiliaryLine v-if="item.key == 'row-line'" :value="item.com_data"></componentDiyAuxiliaryLine>
+                <componentDiyRichText v-if="item.key == 'rich-text'" :value="item.com_data"></componentDiyRichText>
             </view>
         </view>
         <componentDiyFooter :value="value.footer.com_data" @footer-height="footer_height_computer"></componentDiyFooter>
@@ -17,9 +20,10 @@
     import componentDiyHeader from '@/components/diy/header';
     import componentDiyFooter from '@/components/diy/footer';
     import componentDiySearch from '@/components/diy/search';
-    import componentDiyAuxiliaryLine from '@/components/diy/auxiliary-line';
     import componentFloatWindow from '@/components/diy/float-window';
     import componentTextTitle from '@/components/diy/text-title';
+    import componentDiyAuxiliaryLine from '@/components/diy/auxiliary-line';
+    import componentDiyRichText from '@/components/diy/rich-text.vue';
     export default {
         name: 'diy',
         props: {
@@ -32,9 +36,9 @@
             componentDiyHeader,
             componentDiyFooter,
             componentDiySearch,
-            componentDiyAuxiliaryLine,
+            componentDiyRichText,
             componentFloatWindow,
-            componentTextTitle
+            componentTextTitle,
         },
         data() {
             return {
