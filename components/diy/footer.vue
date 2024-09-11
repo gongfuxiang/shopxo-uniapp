1967d1184d8289c9c2cffe20d728a5e95355b549
sws <1141121512@qq.com>
2024-09-11 18:52:08 +0800

    1.diy---- 热区

--- components/diy/footer.vue
+++ components/diy/footer.vue
@@ -3,7 +3,7 @@
     <view class="footer-nav flex-row jc-c align-c bottom-line-exclude">
         <view class="footer-nav-content flex-row jc-c align-c wh" :style="style_container">
             <ul class="flex-row jc-sa align-c wh padding-0">
-                <li v-for="(item, index) in nav_content" :key="index" class="flex-1 flex-col jc-c align-c gap-5" :data-index="index" :data-link="item.link.page" @click="handle_event">
+                <li v-for="(item, index) in nav_content" :key="index" class="flex-1 flex-col jc-c align-c gap-5" :data-index="index" :data-value="item.link.page" @click="url_event">
                     <view v-if="nav_style !== 2" class="img pr">
                         <view class="img-item pa border-radius-xs animate-linear" :class="is_active != index ? 'active' : ''">
                             <img :src="item.img[0].url" class="wh-auto ht-auto" />
@@ -65,10 +65,10 @@
                 }
                 this.$emit('footer-height', footer_height);
             },
-            handle_event(e) {
-                console.log(e);
+            // 跳转链接
+            url_event(e) {
                 this.is_active = e.currentTarget.dataset.index;
-                app.globalData.url_open(e.currentTarget.dataset.link);
+                app.globalData.url_event(e);
             },
         },
     };
