2463513a01e4641e3fc785589391943404af321c
sws <1141121512@qq.com>
2024-09-28 10:16:32 +0800

    1.diy---置顶开发

--- components/diy/modules/tabs-view.vue
+++ components/diy/modules/tabs-view.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- 文章列表  66rpx是自定义顶部导航栏的高度-->
-    <uv-sticky :disabled="!propIsTop" :offset-top="propTop" :zIndex="propZIndex" :customNavHeight="propCustomNavHeight" :bgColor="propTabsBackground" :style="propStyle">
-        <view class="flex-row gap-10 jc-sb align-c">
+    <uv-sticky :disabled="!propIsTop" :offset-top="propTop" :zIndex="propZIndex" :customNavHeight="propCustomNavHeight">
+        <view class="flex-row gap-10 jc-sb align-c" :style="propStyle + 'background:' + propTabsBackground">
             <view class="tabs flex-1 flex-width">
                 <scroll-view :scroll-x="true" :show-scrollbar="false" :scroll-with-animation="true" :scroll-into-view="'one-nav-item-' + active_index" class="wh-auto">
                     <view class="flex-row" :style="'column-gap: ' + tabs_spacing + 'rpx;'">
