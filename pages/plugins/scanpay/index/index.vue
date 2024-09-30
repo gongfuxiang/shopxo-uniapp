49e95b04f2b5fa2acd92238dc928bd85d746035e
gongfuxiang <fuxiang.gong@qq.com>
2024-09-30 10:19:03 +0800

    底部浮动导航处理

--- pages/plugins/scanpay/index/index.vue
+++ pages/plugins/scanpay/index/index.vue
@@ -40,7 +40,7 @@
                     </view>
                 </view>
             </view>
-            <view class="bottom-fixed sub-key">
+            <view class="bottom-fixed sub-key" :style="bottom_fixed_style">
                 <view class="tc text-size-xs spacing-mb">
                     <block v-if="!form.note">
                         <view class="cr-blue" @tap="add_desc_event">{{$t('index.index.1e582h')}}</view>
@@ -137,11 +137,11 @@
         data() {
             return {
                 theme_view: app.globalData.get_theme_value_view(),
-                // 基础配置
                 currency_symbol: app.globalData.currency_symbol(),
                 data: null,
                 data_list_loding_status: 1,
                 data_list_loding_msg: '',
+                bottom_fixed_style: '',
                 form_submit_loading: false,
                 checked: 0,
                 is_more: false,
