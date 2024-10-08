baeeca4c6cdea22b2fb7bb910aa80137e659c8dd
gongfuxiang <fuxiang.gong@qq.com>
2024-10-08 22:54:51 +0800

    商品分类购物车导航优化

--- pages/goods-category/goods-category.vue
+++ pages/goods-category/goods-category.vue
@@ -329,20 +329,20 @@
                                 </view>
                             </block>
                             <!-- 购物车底部导航 -->
-                            <view class="botton-nav round pa oh flex-row jc-sb align-c" :style="botton_nav_style+(cart_status ? 'z-index:5;' : '')">
+                            <view class="botton-nav bg-white round pa oh padding-sm flex-row jc-sb align-c" :style="botton_nav_style+(cart_status ? 'z-index:5;' : '')">
                                 <view class="flex-row align-c flex-1 flex-width">
-                                    <view class="cart pr cp top-sm" @tap="cart_event">
+                                    <view class="cart pr cp top-sm padding-left-lg" @tap="cart_event">
                                         <iconfont name="icon-applet-shop-acquiesce" size="36rpx" color="#666"></iconfont>
                                         <view v-if="(cart || null) != null && (cart.buy_number || 0) != 0" class="badge-icon pa">
                                             <component-badge :propNumber="cart.buy_number"></component-badge>
                                         </view>
                                     </view>
-                                    <view class="cart-total-price single-text fw-b cr-red flex-1">
+                                    <view class="cart-total-price single-text padding-left-lg fw-b cr-red flex-1">
                                         <text class="text-size-xss">{{ currency_symbol }}</text>
                                         <text class="text-size-lg">{{ (cart || null) == null ? 0 : cart.total_price || 0 }}</text>
                                     </view>
                                 </view>
-                                <button type="default" size="mini" hover-class="none" @tap="buy_submit_event" class="text-size-md radius-0 bg-main cr-white">{{ $t('goods-category.goods-category.44f1ww') }}</button>
+                                <button type="default" size="mini" hover-class="none" class="bg-main cr-white text-size-md round margin-right-xs" @tap="buy_submit_event">{{ $t('goods-category.goods-category.44f1ww') }}</button>
                             </view>
                         </block>
                     </view>
@@ -682,18 +682,18 @@
                 // 左侧
                 var left_style = '';
                 if(this.category_goods_is_show_cart_nav == 1) {
-                    left_style = 'height: calc(100% - 100rpx - '+(this.footer_height_value+20)+'rpx);';
+                    left_style = 'height: calc(100% - 120rpx - '+(this.footer_height_value+20)+'rpx);';
                 }
                 // 右侧
                 var right_style = '';
                 if(this.category_goods_is_show_cart_nav == 1 && this.common_site_type != 1) {
-                    right_style = 'padding-bottom: calc(105rpx + '+(this.footer_height_value+20)+'rpx);';
+                    right_style = 'padding-bottom: calc(120rpx + '+(this.footer_height_value+20)+'rpx);';
                 }
                 this.setData({
                     left_content_actual_style: left_style,
                     right_content_actual_style: right_style,
                     botton_nav_style: 'bottom: calc(20rpx + '+this.footer_height_value+'rpx);',
-                    cart_content_style: 'bottom: calc(130rpx + '+this.footer_height_value+'rpx);',
+                    cart_content_style: 'bottom: calc(150rpx + '+this.footer_height_value+'rpx);',
                 });
             },
 
