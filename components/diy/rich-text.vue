4d38c599874fa311312efdc70d820e16bd97fcc2
sws <1141121512@qq.com>
2024-10-15 18:36:57 +0800

    1.bug测试修改

--- components/diy/rich-text.vue
+++ components/diy/rich-text.vue
@@ -45,7 +45,6 @@
                     style_container: common_styles_computer(new_style.common_style),
                     style_img_container: common_img_computer(new_style.common_style),
                 });
-                console.log(new_content.html);
             },
         },
     };
