ecba5dea0ee931bba79fee71e5ff3b8fa25bb90b
于肖磊 <18851179580@163.com>
2024-12-16 15:57:31 +0800

    修改用户编辑弹出框

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -448,7 +448,8 @@
             this.init();
             // 初始化配置
             this.init_config();
-
+            debugger;
+            console.log(this.$refs.common);
             // 公共onshow事件
             if ((this.$refs.common || null) != null) {
                 this.$refs.common.on_show();
