16c410d0e88d4115234705cd13c1acb74dcfd2bb
sws <1141121512@qq.com>
2024-12-18 17:52:31 +0800

    1.留言更新

--- pages/plugins/ask/user-detail/user-detail.vue
+++ pages/plugins/ask/user-detail/user-detail.vue
@@ -1,47 +1,19 @@
 <template>
     <view :class="theme_view">
-        <block v-if="data_list_loding_status != 1 && Object.keys(detail_data.length !== 0)">
-            <component-nav-back></component-nav-back>
-            <view class="weixin-nav-padding-top">
-                <view class="padding-top-xxxl">
-                    <view class="pa top-0 left-0 right-0 nav-top pa-w">
-                        <image mode="widthFix" :src="ask_static_url + 'nav-top.png'" class="wh-auto"></image>
-                    </view>
-                    <view class="padding-main pr margin-top-xxxl">
-                        <view class="bg-white border-radius-main padding-main text-size">
-                            <view class="fw-b text-size-lg spacing-mb">{{$t('user-detail.user-detail.o92ral')}}</view>
-                            <view v-if="detail_data.user && detail_data.user.user_name_view" class="spacing-mb">
-                                <view class="cr-grey-9">{{$t('user-detail.user-detail.k5867n')}}</view>
-                                <view class="margin-top-xs">{{ detail_data.user.user_name_view }}</view>
-                            </view>
-                            <view v-if="detail_data.tel" class="spacing-mb">
-                                <view class="cr-grey-9">{{$t('user-detail.user-detail.gfe703')}}</view>
-                                <view class="margin-top-xs">{{ detail_data.tel }}</view>
-                            </view>
-                            <view v-if="detail_data.title" class="spacing-mb">
-                                <view class="cr-grey-9">{{$t('user-detail.user-detail.uy6lrz')}}</view>
-                                <view class="margin-top-xs">{{ detail_data.title }}</view>
-                            </view>
-                            <view v-if="detail_data.content" class="spacing-mb">
-                                <view class="cr-grey-9">{{$t('user-detail.user-detail.7cufw6')}}</view>
-                                <view class="margin-top-xs">{{ detail_data.content }}</view>
-                            </view>
-                            <view v-if="detail_data.reply" class="spacing-mb">
-                                <view class="cr-grey-9">{{$t('user-detail.user-detail.u411r2')}}</view>
-                                <view class="margin-top-xs">{{ detail_data.reply }}</view>
-                            </view>
-                            <view v-if="detail_data.reply_time_time" class="spacing-mb">
-                                <view class="cr-grey-9">{{$t('user-detail.user-detail.teo6qd')}}</view>
-                                <view class="margin-top-xs">{{ detail_data.reply_time_time }}</view>
-                            </view>
-                            <view v-if="detail_data.add_time_time" class="spacing-mb">
-                                <view class="cr-grey-9">{{$t('user-order-detail.user-order-detail.h2c78h')}}</view>
-                                <view class="margin-top-xs">{{ detail_data.add_time_time }}</view>
-                            </view>
-                        </view>
-                    </view>
+        <block v-if="data_list_loding_status != 1 && data.length != 0">
+            <component-panel-content :propData="data" :propDataField="field_list" propExcludeField="content" propTitle="详情"></component-panel-content>
+
+            <component-panel-content propTitle="提问内容">
+                <view class="item br-b-f5 oh padding-vertical-main">
+                    <mp-html :content="data.content"></mp-html>
                 </view>
-            </view>
+            </component-panel-content>
+
+            <component-panel-content propTitle="回复内容">
+                <view class="item br-b-f5 oh padding-vertical-main">
+                    <mp-html :content="data.reply"></mp-html>
+                </view>
+            </component-panel-content>
         </block>
         <view v-else>
             <!-- 提示信息 -->
@@ -57,13 +29,15 @@
     import componentCommon from '@/components/common/common';
     import componentNavBack from '@/components/nav-back/nav-back';
     import componentNoData from '@/components/no-data/no-data';
+    import componentPanelContent from '@/components/panel-content/panel-content';
 
     export default {
         data() {
             return {
                 theme_view: app.globalData.get_theme_value_view(),
                 ask_static_url: app.globalData.get_static_url('ask', true),
-                detail_data: {},
+                data: {},
+                field_list: [],
                 data_list_loding_status: 1,
                 params: '',
             };
@@ -73,6 +47,7 @@
             componentCommon,
             componentNavBack,
             componentNoData,
+            componentPanelContent,
         },
 
         onLoad(params) {
@@ -132,7 +107,8 @@
                         uni.stopPullDownRefresh();
                         if (res.data.code == 0) {
                             this.setData({
-                                detail_data: res.data.data || {},
+                                data: res.data.data.data || [],
+                                field_list: res.data.data.field_list || [],
                                 data_list_loding_status: 3,
                             });
                         } else {
