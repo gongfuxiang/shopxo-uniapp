bbecb281431b12304d8f9850619ba58d306cc3ca
于肖磊 <18851179580@163.com>
2024-12-24 15:32:56 +0800

    修改页面显示内容

--- components/diy/modules/data-magic/custom/index.vue
+++ components/diy/modules/data-magic/custom/index.vue
@@ -1,41 +1,45 @@
 <template>
-    <view v-if="data_source_content_list.length > 0 && form.data_source_direction == 'vertical'">
-        <view class="flex-row flex-wrap" :style="'row-gap:' + new_style.row_gap + 'px;column-gap:' + new_style.column_gap + 'px;'">
-            <view v-for="(item, index) in data_source_content_list" :key="index" class="ht-auto" :style="gap_width">
-                <view v-for="(item1, index1) in item.split_list" :key="index1">
-                    <view :style="style_container">
-                        <view class="wh-auto ht-auto oh" :style="style_img_container">
-                            <dataRendering :propKey="propKey" :propCustomList="form.custom_list" :propSourceList="item1" :propSourceType="form.data_source" :propDataHeight="form.height" :propScale="scale" :propDataIndex="index" :propDataSplitIndex="index1" :propIsCustom="form.is_custom_data == '1'" :propShowData="show_data" @url_event="url_event"></dataRendering>
+    <view :style="style_content_container">
+        <view :style="style_content_img_container">
+            <view v-if="data_source_content_list.length > 0 && form.data_source_direction == 'vertical'">
+                <view class="flex-row flex-wrap" :style="'row-gap:' + new_style.row_gap + 'px;column-gap:' + new_style.column_gap + 'px;'">
+                    <view v-for="(item, index) in data_source_content_list" :key="index" class="ht-auto" :style="gap_width">
+                        <view v-for="(item1, index1) in item.split_list" :key="index1">
+                            <view :style="style_container">
+                                <view class="wh-auto ht-auto oh" :style="style_img_container">
+                                    <dataRendering :propKey="propKey" :propCustomList="form.custom_list" :propSourceList="item1" :propSourceType="form.data_source" :propDataHeight="form.height" :propScale="scale" :propDataIndex="index" :propDataSplitIndex="index1" :propIsCustom="form.is_custom_data == '1'" :propShowData="show_data" @url_event="url_event"></dataRendering>
+                                </view>
+                            </view>
                         </view>
                     </view>
                 </view>
             </view>
-        </view>
-    </view>
-    <div v-else-if="data_source_content_list.length > 0 && ['vertical-scroll', 'horizontal'].includes(form.data_source_direction)" class="oh pr">
-        <swiper class="w flex" circular="true" :vertical="form.data_source_direction != 'horizontal'" :autoplay="new_style.is_roll == '1'" :interval="new_style.interval_time * 1000" :duration="500" :display-multiple-items="slides_per_view" :style="{ width: '100%', height: swiper_height + 'px' }" @change="slideChange">
-            <swiper-item v-for="(item, index) in data_source_content_list" :key="index">
-                <view :class="form.data_source_direction != 'horizontal' ? '' : 'flex-row'" :style="form.data_source_direction == 'horizontal' ? 'column-gap:' + new_style.column_gap + 'px;' : ''">
-                    <view v-for="(item1, index1) in item.split_list" :key="index1" :style="style_container + swiper_width + (form.data_source_direction == 'horizontal' ? gap_width : 'margin-bottom:' + content_outer_spacing_magin)">
-                        <div class="wh-auto ht-auto oh" :style="style_img_container">
-                            <dataRendering :propKey="propKey" :propCustomList="form.custom_list" :propSourceList="item1" :propSourceType="form.data_source" :propDataHeight="form.height" :propScale="scale" :propDataIndex="index" :propDataSplitIndex="index1" :propIsCustom="form.is_custom_data == '1'" :propShowData="show_data" @url_event="url_event"></dataRendering>
-                        </div>
+            <div v-else-if="data_source_content_list.length > 0 && ['vertical-scroll', 'horizontal'].includes(form.data_source_direction)" class="oh pr">
+                <swiper class="w flex" circular="true" :vertical="form.data_source_direction != 'horizontal'" :autoplay="new_style.is_roll == '1'" :interval="new_style.interval_time * 1000" :duration="500" :display-multiple-items="slides_per_view" :style="{ width: '100%', height: swiper_height + 'px' }" @change="slideChange">
+                    <swiper-item v-for="(item, index) in data_source_content_list" :key="index">
+                        <view :class="form.data_source_direction != 'horizontal' ? '' : 'flex-row'" :style="form.data_source_direction == 'horizontal' ? 'column-gap:' + new_style.column_gap + 'px;' : ''">
+                            <view v-for="(item1, index1) in item.split_list" :key="index1" :style="style_container + swiper_width + (form.data_source_direction == 'horizontal' ? gap_width : 'margin-bottom:' + content_outer_spacing_magin)">
+                                <div class="wh-auto ht-auto oh" :style="style_img_container">
+                                    <dataRendering :propKey="propKey" :propCustomList="form.custom_list" :propSourceList="item1" :propSourceType="form.data_source" :propDataHeight="form.height" :propScale="scale" :propDataIndex="index" :propDataSplitIndex="index1" :propIsCustom="form.is_custom_data == '1'" :propShowData="show_data" @url_event="url_event"></dataRendering>
+                                </div>
+                            </view>
+                        </view>
+                    </swiper-item>
+                </swiper>
+            </div>
+            <view v-else>
+                <view :style="style_container">
+                    <view class="wh-auto ht-auto oh" :style="style_img_container">
+                        <dataRendering :propKey="propKey" :propCustomList="form.custom_list" :propDataHeight="form.height" :propScale="scale" @url_event="url_event"></dataRendering>
                     </view>
                 </view>
-            </swiper-item>
-        </swiper>
-    </div>
-    <view v-else>
-        <view :style="style_container">
-            <view class="wh-auto ht-auto oh" :style="style_img_container">
-                <dataRendering :propKey="propKey" :propCustomList="form.custom_list" :propDataHeight="form.height" :propScale="scale" @url_event="url_event"></dataRendering>
             </view>
         </view>
     </view>
 </template>
 
 <script>
-import { padding_computer, isEmpty, margin_computer, gradient_computer, radius_computer, background_computer } from '@/common/js/common/common.js';
+import { padding_computer, isEmpty, margin_computer, gradient_computer, radius_computer, background_computer, common_styles_computer, common_img_computer } from '@/common/js/common/common.js';
 import dataRendering from '@/components/diy/modules/custom/data-rendering.vue';
 const app = getApp();
 
@@ -87,6 +91,29 @@ export default {
             show_data: { data_key: 'id', data_name: 'name' },
             gap_width: '',
             content_outer_spacing_magin: '0rpx',
+            defalt_style: {
+                color_list: [{ color: '', color_percentage: undefined }],
+                direction: '180deg',
+                background_img_style: '2',
+                background_img: [],
+                radius: 0,
+                radius_top_left: 0,
+                radius_top_right: 0,
+                radius_bottom_left: 0,
+                radius_bottom_right: 0,
+                padding: 0,
+                padding_top: 0,
+                padding_bottom: 0,
+                padding_left: 0,
+                padding_right: 0,
+                margin: 0,
+                margin_top: 0,
+                margin_bottom: 0,
+                margin_left: 0,
+                margin_right: 0,
+            },
+            style_content_container: '',
+            style_content_img_container: '',
         };
     },
     watch: {
@@ -159,12 +186,15 @@ export default {
                 let gap = (new_style.column_gap * (model_number - 1)) / model_number;
                 // 横向的时候，根据选择的行数和每行显示的个数来区分具体是显示多少个
                 const swiper_width = (new_form.data_source_direction == 'horizontal' && new_style.rolling_fashion != 'translation') ? `width: ${ 100 / new_form.data_source_carousel_col }%;`: 'width: 100%;';
+                const content_style = !isEmpty(new_style.data_content_style)? new_style.data_content_style : this.defalt_style;
                 this.setData({
                     form: new_form,
                     new_style: new_style,
                     div_width: width,
                     scale: new_scale,
                     custom_list_length: new_form.custom_list.length - 1,
+                    style_content_container: common_styles_computer(content_style),
+                    style_content_img_container: common_img_computer(content_style),
                     style_container: gradient_computer(style_data) + radius_computer(data_radius) + margin_computer(data_chunk_margin), // 用于样式显示
                     style_img_container: padding_computer(data_chunk_padding) + background_computer(style_img_data) + 'box-sizing: border-box;',
                     data_source_content_list: new_list,
