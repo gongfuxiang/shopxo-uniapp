ae5f12a89ce52f72d7e684c7d313138640ca9a6b
gongfuxiang <fuxiang.gong@qq.com>
2025-01-18 18:51:28 +0800

    优惠券新增详情

--- components/bottom-line/bottom-line.vue
+++ components/bottom-line/bottom-line.vue
@@ -2,9 +2,9 @@
     <view :class="theme_view">
         <view v-if="(propStatus || false)" class="data-bottom-line">
             <view class="bottom-exclude">
-                <view class="item left"></view>
-                <view class="item msg">{{propMsg || $t('bottom-line.bottom-line.44bct2')}}</view>
-                <view class="item right"></view>
+                <view class="line-item left"></view>
+                <view class="line-item msg">{{propMsg || $t('bottom-line.bottom-line.44bct2')}}</view>
+                <view class="line-item right"></view>
             </view>
         </view>
     </view>
@@ -33,24 +33,24 @@
     .data-bottom-line .bottom-exclude {
         padding-bottom: env(safe-area-inset-bottom);
     }
-    .data-bottom-line .item {
+    .data-bottom-line .line-item {
         width: 33.3%;
     }
-    .data-bottom-line .left,
-    .data-bottom-line .right {
+    .data-bottom-line .line-item.left,
+    .data-bottom-line .line-item.right {
         margin-top: 8px;
         border-bottom: 1px solid #e1e1e1;
     }
-    .data-bottom-line .msg {
+    .data-bottom-line .line-item.msg {
         color: #999;
         text-align: center;
         font-size: 24rpx;
     }
-    .data-bottom-line .left,
-    .data-bottom-line .msg {
+    .data-bottom-line .line-item.left,
+    .data-bottom-line .line-item.msg {
         float: left;
     }
-    .data-bottom-line .right {
+    .data-bottom-line .line-item.right {
         float: right;
     }
 </style>
