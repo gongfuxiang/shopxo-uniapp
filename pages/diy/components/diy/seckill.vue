b692eba2efb95928711c76e028fd6b6751a15606
gongfuxiang <fuxiang.gong@qq.com>
2025-06-11 15:46:32 +0800

    秒杀标题加粗

--- pages/diy/components/diy/seckill.vue
+++ pages/diy/components/diy/seckill.vue
@@ -7,7 +7,7 @@
                         <view :class="['flex-row align-c', { 'gap-10': form.theme != '1', 'jc-sb wh-auto': form.theme == '2' }]">
                             <view class="seckill-title">
                                 <image-empty v-if="form.title_type == 'image'" :propImageSrc="form.title_src[0]" propImgFit="heightFix" propErrorStyle="width:42rpx; height: 20rpx;"></image-empty>
-                                <text v-else :style="{ color: new_style.title_color, 'font-size': new_style.title_size * 2 + 'rpx', 'line-height': '42rpx', 'font-weight': 600 }">{{ form.title_text }}</text>
+                                <text v-else :style="{ color: new_style.title_color, 'font-size': new_style.title_size * 2 + 'rpx', 'line-height': '42rpx', 'font-weight': 'bold' }">{{ form.title_text }}</text>
                             </view>
                             <view v-if="form.theme == '1'" class="padding-horizontal-sm cr-white">|</view>
                             <view v-if="intervalId != undefined" class="flex-row align-c gap-4">
