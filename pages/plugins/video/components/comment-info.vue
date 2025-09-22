78812c06a90e7aa9489c18500c143f7bc130cf6b
于肖磊 <18851179580@163.com>
2025-09-22 09:26:18 +0800

    修改评论区处理

--- pages/plugins/video/components/comment-info.vue
+++ pages/plugins/video/components/comment-info.vue
@@ -1,15 +1,15 @@
 <template> 
     <view class="flex-row align-s gap-10 wh-auto ht-auto"> 
         <image class="comment-avatar" :src="propsComment.userHead" mode="aspectFill"></image>
-        <view class="comment-info">
+        <view class="comment-info" @tap="comment_reply">
             <view class="comment-user">{{ propsComment.userNick }}</view>
             <view class="comment-text">{{ propsComment.content }}</view>
             <view class="comment-operation flex-row align-c jc-sb">
                 <view class="comment-operation-left flex-row align-c gap-10">
                     <view class="comment-time">{{ propsComment.time }}</view>
-                    <view class="comment-reply" @click="comment_reply">{{ propsReplyContent }}</view>
+                    <view class="comment-reply">{{ propsReplyContent }}</view>
                 </view>
-                <view class="comment-operation-right flex-row align-c gap-5" @click="comment_like">
+                <view class="comment-operation-right flex-row align-c gap-5" @tap.stop="comment_like">
                     <iconfont name="icon-givealike-o-fine" color="#000" size="28rpx" />
                     <view class="comment-like-num">{{ propsComment.likeNum || 0 }}</view>
                 </view>
@@ -38,9 +38,11 @@
             }
         },
         methods: {
+            // 回复
             comment_reply() {
                 this.$emit('comment_reply', this.propsid);
             },
+            // 点赞
             comment_like() {
                 this.$emit('comment_like', this.propsid);
             }
