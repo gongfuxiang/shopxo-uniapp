0ac506c77ba9066755ab5ff4180c3dfd385fec42
gongfuxiang <fuxiang.gong@qq.com>
2025-10-27 18:38:30 +0800

    debug

---
+++ pages/plugins/live/pull/pull.vue
@@ -0,0 +1,33 @@
+<template>
+    <view>
+        <live-player
+          src="rtmp://live-pull.shopxo.vip/68f764013572f9240ca7ce6c/shopxo"
+          mode="live"
+          autoplay
+          @statechange="statechange"
+          @error="error"
+          style="width: 400px; height: 525px;"
+        />
+    </view>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        statechange(e){
+            console.log('live-player code:', e.detail.code)
+        },
+        error(e){
+            console.error('live-player error:', e.detail.errMsg)
+        }
+    }
+}
+</script>
+
+<style>
+</style>
