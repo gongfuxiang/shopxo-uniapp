ed68752dd53303386240f5738bfc4d1efde025cc
于肖磊 <18851179580@163.com>
2026-01-08 11:49:53 +0800

    修改显示

--- pages/plugins/live/pull/pull.vue
+++ pages/plugins/live/pull/pull.vue
@@ -1,7 +1,6 @@
 <template>
     <view :class="theme_view + ' pr'">
         <view class="pr live-bg" @click="handle_click" @touchend="handle_touch_end" :data-ignore="false">
-            <text>{{ !is_live_ended && is_muted_auto_play_error && !live_be_right_back_error }}</text>
             <component-live-video v-if="!is_live_ended" ref="liveVideo" :propSrc="live_video_src || 'http://live-pull-all.shopxo.vip/68f764013572f9240ca7ce6c/shopxo.m3u8'" @ended="ended" @loadedmetadata="loadedmetadata" @mutedAutoPlaySuccess="muted_auto_play_success" @mutedTap="muted_tap" @mutedAutoPlayError="muted_auto_play_error"></component-live-video>
             <!-- 简化版点赞效果组件 -->
             <component-full-screen-like-effect ref="fullScreenLikeEffect" :propCustomImages="like_show_imgs"></component-full-screen-like-effect>
@@ -142,7 +141,6 @@
         z-index: 11;
         width: 100%;
         height: 100%;
-        background: red;
         .component-icon-play {
             height: 200rpx;
             width: 200rpx;
