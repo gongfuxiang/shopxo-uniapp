1858f78661d93c4e73cbf178dab1b2a1fb012031
gongfuxiang <fuxiang.gong@qq.com>
2026-05-18 21:05:52 +0800

    v6.9 log

--- App.vue
+++ App.vue
@@ -23,10 +23,10 @@
                 application_logo: '',
 
                 // 版本号、如: v1.0.0
-                version: 'v6.8.0',
+                version: 'v6.9.0',
 
                 // app版本信息、如: v1.0.0 20180118
-                app_version_info: 'v6.8.0 20260305',
+                app_version_info: 'v6.9.0 20260525',
 
                 // 货币价格符号
                 currency_symbol: '￥',
